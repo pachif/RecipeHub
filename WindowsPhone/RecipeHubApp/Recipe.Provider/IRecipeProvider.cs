62b8014f7b8ca39fa33f7bdb8260a82891e21750
pachif <pachif81@yahoo.com.ar>
2015-11-06 18:58:50 -0300

    missing files

---
+++ WindowsPhone/RecipeHubApp/Recipe.Provider/IRecipeProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recipes.Provider
+{
+    public interface IRecipeProvider
+    {
+        string ProviderName { get; }
+        void ObtainRecipeById(string id);
+        void SearchRecipeByName(string name);
+        void SearchRecipeByName(string name, int page);
+        void ObtainMostRecents();
+
+        event EventHandler<ResultEventArgs> ProcessEnded;
+    }
+}
